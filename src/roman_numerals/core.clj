d8cae6ae358c28fbb35568a3904a13bec77e5b8c
nawhi <25394965+nawhi@users.noreply.github.com>
2021-08-06 12:39:12 +0100

    Create fully consistent cond block

--- src/roman_numerals/core.clj
+++ src/roman_numerals/core.clj
@@ -5,13 +5,12 @@
   "Convert a number to its equivalent in Roman Numerals"
   [n]
   (cond
-    (= n 4) "IV"
-    (= n 5) "V"
-    (= n 9) "IX"
-    (> n 99) (str "C" (roman-numeral (- n 100)))
-    (> n 49) (str "L" (roman-numeral (- n 50)))
-    (> n 39) (str "XL" (roman-numeral (- n 40)))
-    (> n 9) (str "X" (roman-numeral (- n 10)))
-    (> n 5) (str "V" (roman-numeral (- n 5)))
-    (< n 4) (str/join (repeat n "I"))
+    (>= n 100) (str "C" (roman-numeral (- n 100)))
+    (>= n 50) (str "L" (roman-numeral (- n 50)))
+    (>= n 40) (str "XL" (roman-numeral (- n 40)))
+    (>= n 10) (str "X" (roman-numeral (- n 10)))
+    (= n 9) (str "IX" (roman-numeral (- n 9)))
+    (>= n 5) (str "V" (roman-numeral (- n 5)))
+    (>= n 4) (str "IV" (roman-numeral (- n 5)))
+    :else (str/join (repeat n "I"))
     ))
