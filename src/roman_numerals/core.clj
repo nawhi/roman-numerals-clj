0876218f162b2e277f94bf0400235a232e6e4ebb
nawhi <25394965+nawhi@users.noreply.github.com>
2021-08-06 16:00:14 +0100

    Use hash map instead of cond-cascade

--- src/roman_numerals/core.clj
+++ src/roman_numerals/core.clj
@@ -1,26 +1,24 @@
 (ns roman-numerals.core
   (:require [clojure.string :as str]))
 
-(def NUMERALS {:4   "IV"
-               :5   "V"
-               :9   "IX"
-               :10  "X"
-               :40  "XL"
-               :50  "L"
-               :90  "XC"
-               :100 "C"})
+(def RAW-NUMERALS {'1   "I",
+                   '4   "IV",
+                   '5   "V",
+                   '9   "IX",
+                   '10  "X",
+                   '40  "XL",
+                   '50  "L",
+                   '90  "XC",
+                   '100 "C"})
+(def NUMERALS (into (sorted-map-by >) RAW-NUMERALS))
 
 (defn roman-numeral
   "Convert a number to its Roman Numeral equivalent"
   [n]
-  (cond
-    (>= n 100) (str "C" (roman-numeral (- n 100)))
-    (>= n 90) (str "XC" (roman-numeral (- n 100)))
-    (>= n 50) (str "L" (roman-numeral (- n 50)))
-    (>= n 40) (str "XL" (roman-numeral (- n 40)))
-    (>= n 10) (str "X" (roman-numeral (- n 10)))
-    (>= n 9) (str "IX" (roman-numeral (- n 9)))
-    (>= n 5) (str "V" (roman-numeral (- n 5)))
-    (>= n 4) (str "IV" (roman-numeral (- n 5)))
-    :else (str/join (repeat n "I"))
-    ))
+  (loop
+    [n n result ""]
+    (if (zero? n)
+      result
+      (let [entry (filter (fn [[number roman]] (>= n number)) NUMERALS)
+            [[number roman]] entry]
+        (recur (- n number) (str result roman))))))
