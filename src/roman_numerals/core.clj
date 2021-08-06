cd179ef4170616174b4cecc2ed5532b04e28e1a9
nawhi <25394965+nawhi@users.noreply.github.com>
2021-08-06 16:03:08 +0100

    Finish off

--- src/roman_numerals/core.clj
+++ src/roman_numerals/core.clj
@@ -1,20 +1,20 @@
 (ns roman-numerals.core
   (:require [clojure.string :as str]))
 
-(def RAW-NUMERALS {'1   "I",
-                   '4   "IV",
-                   '5   "V",
-                   '9   "IX",
-                   '10  "X",
-                   '40  "XL",
-                   '50  "L",
-                   '90  "XC",
-                   '100 "C"
-                   '400 "CD"
-                   '500 "D"
-                   '900 "CM"
-                   '1000 "M"})
-(def NUMERALS (into (sorted-map-by >) RAW-NUMERALS))
+(def RAW-NUMERALS {'1    "I",
+                   '4    "IV",
+                   '5    "V",
+                   '9    "IX",
+                   '10   "X",
+                   '40   "XL",
+                   '50   "L",
+                   '90   "XC",
+                   '100  "C"
+                   '400  "CD"
+                   '500  "D"
+                   '900  "CM"
+                   '1000 "M"}
+  (def NUMERALS (into (sorted-map-by >) RAW-NUMERALS)))
 
 (defn roman-numeral
   "Convert a number to its Roman Numeral equivalent"
